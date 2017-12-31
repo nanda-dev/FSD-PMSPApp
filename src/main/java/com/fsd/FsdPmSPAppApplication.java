bb33de61e8f7e07b86c8088a308aa6539ebf8127
Nanda.GS <nandagopan.gs@cognizant.com>
2017-12-31 23:06:17 +0530

    Converted to Spring-boot enabled application

---
+++ src/main/java/com/fsd/FsdPmSPAppApplication.java
@@ -0,0 +1,12 @@
+package com.fsd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FsdPmSPAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FsdPmSPAppApplication.class, args);
+	}
+}
